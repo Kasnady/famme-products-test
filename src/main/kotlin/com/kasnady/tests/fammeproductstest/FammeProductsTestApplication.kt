1a71a2ba577dc0646849491b58cdcc92b8006ab2
Arwin <arwin@returnkey.co>
2025-10-04 08:02:50 +0700

    initial project setup using IntellIJIDEA

---
+++ src/main/kotlin/com/kasnady/tests/fammeproductstest/FammeProductsTestApplication.kt
@@ -0,0 +1,11 @@
+package com.kasnady.tests.fammeproductstest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FammeProductsTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<FammeProductsTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/kasnady/tests/fammeproductstest/FammeProductsTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kasnady.tests.fammeproductstest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FammeProductsTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
